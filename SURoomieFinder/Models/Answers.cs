8d735e81bbdb9f2bab7100f7b183966dc3614cc3
srf98 <33579952+srf98@users.noreply.github.com>
2017-11-11 15:44:44 -0800

    Finished sublists of answers class

--- SURoomieFinder/Models/Answers.cs
+++ SURoomieFinder/Models/Answers.cs
@@ -12,11 +12,11 @@ namespace SURoomieFinder.Models
 			"Capitol Hill", "Madrona", "Queen Anne", "Ballard", "Fremont", "University District" });
 		private static List<string> PriceAnswers = new List<string>(new string[] { "$0 - $500", "$501 - $800", "$801 - $1,100", "$1,100 - $1,500", "$1,500+" });
 		private static List<string> TypeAnswers = new List<string>(new string[] { "Small apartment", "High rise", "Duplex", "Full house" });
-		private static List<string> PetAnswers = new List<string>(new string[] { "None", "Cats", "Dogs", "Cats and Dogs", "Other" });
-		private static List<string> DrinkingAnswers = new List<string>(new string[] {"No, and I DO NOT mind if my roomie does",
-			"No, and I DO mind if my roomie does", "Yes (Occasionally)", "Yes (Often)" });
-		private static List<string> SmokingAnswers = new List<string>(new string[] {"No, and I DO NOT mind if my roomie does",
-			"No, and I DO mind if my roomie does", "Yes (Occasionally)", "Yes (Often)" });
+		private static List<string> PetAnswers = new List<string>(new string[] { "None", "Fish", "Amphibian", "Reptile", "Bird", "Vermin", "Cats", "Dogs", "Cats and Dogs"});
+		private static List<string> DrinkingAnswers = new List<string>(new string[] {"No, and I DO mind if my roomie does",
+			"No, and I DO NOT mind if my roomie does", "Yes (Occasionally)", "Yes (Often)" });
+		private static List<string> SmokingAnswers = new List<string>(new string[] {"No, and I DO mind if my roomie does",
+			"No, and I DO NOT mind if my roomie does", "Yes (Occasionally)", "Yes (Often)" });
 		private static List<string> FourTwentyAnswers = new List<string>(new string[] {"No, and I DO NOT mind if my roomie is",
 			"No, and I DO mind if my roomie is", "Yes (Occasionally)", "Yes (Often)" });
 		private static List<string> SubstancesAnswers = new List<string>(new string[] {"No, and I DO NOT mind if my roomie does",
@@ -28,6 +28,13 @@ namespace SURoomieFinder.Models
 		private static List<string> TimeAnswers = new List<string>(new string[] {"Spend very little time at home", "Spend some leisure time at home",
 			"Spend a good deal of leisure time at home", "Spend majority of leisure time at home" });
 		private static List<string> DisabilityAnswers = new List<string>(new string[] { "Yes", "No" });
+		private static List<string> LGBTQAnswers = new List<string>(new string[] { "Yes", "No" });
+		private static List<string> GenderAnswers = new List<string>(new string[] { "Yes", "No" });
+		private static List<string> NoiseAnswers = new List<string>(new string[] {"Very quiet", "Somewhat quiet", "Neither loud nor quiet",
+			"Somewhat noisy", "Very noisy"});
+
+	     
+
 
 	}
 }
