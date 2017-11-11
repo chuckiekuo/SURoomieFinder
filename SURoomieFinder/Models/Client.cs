547645d6001235165794d247f1e22b708957fa79
Kuo, Chuck <kuoc1@seattleu.edu>
2017-11-11 13:08:11 -0800

    Added nuget packages
    Bootstrap, entity

---
+++ SURoomieFinder/Models/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace SURoomieFinder.Models
+{
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string Age { get; set; }
+        public string Major { get; set; }
+        public string Job { get; set; }
+        public string Phone { get; set; }
+        public string Gender { get; set; }
+        public string Hometown { get; set; }
+        
+
+    }
+
+    public class ClientDbContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+    }
+}
