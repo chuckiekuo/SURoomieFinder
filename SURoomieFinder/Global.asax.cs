546c380cf49affed5f3002ade2f3439b8c519d57
Kuo, Chuck <kuoc1@seattleu.edu>
2017-11-12 11:37:31 -0800

    Commit

--- SURoomieFinder/Global.asax.cs
+++ SURoomieFinder/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using SURoomieFinder.Models;
 
 namespace SURoomieFinder
 {
@@ -16,6 +18,8 @@ namespace SURoomieFinder
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer<GuestDbContext>(null);
+
         }
     }
 }
--- SURoomieFinder/Models/DummyGuest.cs
+++ SURoomieFinder/Models/DummyGuest.cs
@@ -11,7 +11,19 @@ namespace SURoomieFinder.Models
         public int Id;
         public string Q1 { get; set; }
         public string Q2 { get; set; }
-        public string Q3 { get; set; } Q4, Q5, Q6, Q7, Q8, Q9, Q10, Q11, Q12, Q13, Q14, Q15;
+        public string Q3 { get; set; }
+        public string Q4 { get; set; }
+        public string Q5 { get; set; }
+        public string Q6 { get; set; }
+        public string Q7 { get; set; }
+        public string Q8 { get; set; }
+        public string Q9 { get; set; }
+        public string Q10 { get; set; }
+        public string Q11 { get; set; }
+        public string Q12 { get; set; }
+        public string Q13 { get; set; }
+        public string Q14 { get; set; }
+        public string Q15 { get; set; }
            
         public DummyGuest(Guest guest)
         {
