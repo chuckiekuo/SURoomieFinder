eec4000e0dc22a9e23020e66fcfdb73910af5869
srf98 <33579952+srf98@users.noreply.github.com>
2017-11-11 17:19:29 -0800

    Finished responses class

--- SURoomieFinder/Models/Responses.cs
+++ SURoomieFinder/Models/Responses.cs
@@ -14,7 +14,66 @@ namespace SURoomieFinder.Models
         "How much time do you spend at home?", "Do you prefer to live in housing with accomodations for people with disabilities?", "Do you prefer to live in housing with exclusively members of the LGBTQ+ community?", "Do you prefer to live with people of the same gender?",
         "What noise level are you comfortable with in your living situation?"});
 
-        [Display(Name = questions[0])]
+        [Display(Name = "Which neighborhood do you want to live in?")]
+		[Range(0,9)]
         public int Question1 { get; set; }
-    }
+
+		[Display(Name = "How much are you willing to spend per month on rent?")]
+		[Range(0, 4)]
+		public int Question2 { get; set; }
+
+		[Display(Name = "What type of housing do you prefer?")]
+		[Range(0, 3)]
+		public int Question3 { get; set; }
+
+		[Display(Name = "What pets do you currently have or plan to acquire?")]
+		[Range(0, 8)]
+		public int Question4 { get; set; }
+
+		[Display(Name = "Do you drink alcohol?")]
+		[Range(0, 3)]
+		public int Question5 { get; set; }
+
+		[Display(Name = "Do you smoke cigarettes?")]
+		[Range(0, 3)]
+		public int Question6 { get; set; }
+
+		[Display(Name = "Are you 420 friendly?")]
+		[Range(0, 3)]
+		public int Question7 { get; set; }
+
+		[Display(Name = "Do you partake in other substances not previously listed?")]
+		[Range(0, 3)]
+		public int Question8 { get; set; }
+
+		[Display(Name = "How would you rate your own cleanliness in a living situation?")]
+		[Range(0, 4)]
+		public int Question9 { get; set; }
+
+		[Display(Name = "What is your comfort with having guests in your living space?")]
+		[Range(0, 3)]
+		public int Question10 { get; set; }
+
+		[Display(Name = "How much time do you spend at home?")]
+		[Range(0, 3)]
+		public int Question11 { get; set; }
+
+		[Display(Name = "Do you prefer to live in housing with accomodations for people with disabilities?")]
+		[Range(0, 1)]
+		public int Question12 { get; set; }
+
+		[Display(Name = "Do you prefer to live in housing with exclusively members of the LGBTQ+ community?")]
+		[Range(0, 1)]
+		public int Question13 { get; set; }
+
+		[Display(Name = "Do you prefer to live with people of the same gender?")]
+		[Range(0, 1)]
+		public int Question14 { get; set; }
+
+		[Display(Name = "What noise level are you comfortable with in your living situation?")]
+		[Range(0, 4)]
+		public int Question15 { get; set; }
+
+
+	}
 }
